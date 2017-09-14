d14f3d073010346f9cac8ffed24de95988aec518
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-14 17:11:21 +0300

    Install flask migrate to handle db migrations

--- manage.py
+++ manage.py
@@ -1,13 +1,20 @@
-
 from flask_script import Manager,Server
+from flask_migrate import Migrate,MigrateCommand
 from app import create_app,db
+from app.models import Lesson,Week
 
 app = create_app('default')
 
 manager = Manager(app)
+migrate = Migrate(app,db)
 
 manager.add_command('server', Server)
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, Lesson = Lesson, Week = Week)
+
 
 if __name__ == '__main__':
     manager.run()
-
