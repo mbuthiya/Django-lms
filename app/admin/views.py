5d62e2d9a8697afcabeb43e9a6c19dfb531a1c2b
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-15 03:29:23 +0300

    create view function for the new lesson

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,5 +1,11 @@
 from . import admin
+from flask import render_template,redirect,url_for
+from .forms import CreateLesson
 
 @admin.route('/dashboard/newLesson',methods = ['GET','POST'])
 def newLesson():
-    
+    form = CreateLesson()
+    if form.validate_on_submit:
+        return redirect(url_for('main.index'))
+
+    return render_template('admin/newLesson.html',form = form)
