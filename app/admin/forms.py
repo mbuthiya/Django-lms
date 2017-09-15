0a4c0284b145ff20025b6e0d9278c9baa8c1a513
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-15 03:25:59 +0300

    create a lesson flask form

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,3 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import Required
-from wtform import SubmitField
+from wtform import SubmitField,SelectField,StringField
+from flask_pagedown.fields import PageDownField
+
+class CreateLesson(FlaskForm):
+    title = StringField("Title for the lesson", validators = [Required()])
+    days = SelectField('Day',choices =[("Monday","Monday"),("Tuesday","Tuesday"),("Wednesday","Wednesday"),("Thursday","Thursday"),("Friday","Friday")])
+    body = PageDownField("Edit your content Markdown", validators = [Required()])
+    submit = SubmitField("Submit Lesson")
