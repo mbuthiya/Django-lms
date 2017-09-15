fc4d68977de66645fc5fe22f8472dbe727587f5a
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-15 10:13:19 +0300

    Update markdown conversion

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,18 @@ from .. import db
 
 @main.route('/')
 def index():
-    week1 =Lesson.query.filter_by(week_number = 1).all()
+    weeks = get_weeks()
+    day = Lesson.query.filter_by(day_number = 1).first()
+    return render_template('main/index.html',weeks=weeks,day=day)
+
+@main.route('/<int:day_num>')
+def day_content(day_num):
+    weeks = get_weeks()
+    day = Lesson.query.filter_by(day_number = day_num).first()
+    return render_template('main/index.html',weeks=weeks,day=day)
+
+def get_weeks():
+    week1 =Lesson.query.filter_by(week_number = 1).order_by(Lesson.day_number).all()
     weeks = {"Introduction To flask":week1}
-    return render_template('main/index.html',weeks=weeks)
+
+    return weeks
--- app/models.py
+++ app/models.py
@@ -18,10 +18,9 @@ class Lesson(db.Model):
 
         allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
         'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
-        'h1', 'h2', 'h3', 'p']
-        target.body_html = bleach.linkify(bleach.clean(
-        markdown(value, output_format='html'),
-        tags=allowed_tags, strip=True))
+        'h1', 'h2', 'h3', 'p','summary']
+        
+        target.body_html =markdown(value, output_format='html')
 
 
     def save_lesson(self):
