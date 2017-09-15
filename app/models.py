f1db9ece4296bfe74964c197e76cbe41e6f17502
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-15 05:47:47 +0300

    Update model to add body html

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from . import db
+from markdown import markdown
+import bleach
 
 class Lesson(db.Model):
     __tablename__ = 'lessons'
@@ -7,9 +9,22 @@ class Lesson(db.Model):
     day_number = db.Column(db.Integer)
     week_number = db.Column(db.Integer)
     body = db.Column(db.String)
+    body_html = db.Column(db.Text)
     lessons = db.Column(db.String)
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+        markdown(value, output_format='html'),
+        tags=allowed_tags, strip=True))
 
 
     def save_lesson(self):
         db.session.add(self)
         db.session.commit()
+        
+db.event.listen(Lesson.body,'set',Lesson.on_changed_body)
