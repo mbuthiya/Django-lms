6f46c9def1cd7af0358267c0aec1004d2fd362cd
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-09-15 10:30:48 +0300

    create migration file

--- app/models.py
+++ app/models.py
@@ -10,21 +10,9 @@ class Lesson(db.Model):
     day_name = db.Column(db.String)
     week_number = db.Column(db.Integer)
     body = db.Column(db.String)
-    body_html = db.Column(db.Text)
     lessons = db.Column(db.String)
-
-    @staticmethod
-    def on_changed_body(target, value, oldvalue, initiator):
-
-        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
-        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
-        'h1', 'h2', 'h3', 'p','summary']
-        
-        target.body_html =markdown(value, output_format='html')
 
 
     def save_lesson(self):
         db.session.add(self)
         db.session.commit()
-
-db.event.listen(Lesson.body,'set',Lesson.on_changed_body)
